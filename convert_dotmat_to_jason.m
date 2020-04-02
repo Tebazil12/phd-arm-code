ade42f9c053225b8d9205ffe02c77feabc63e671
Lizzie Stone <lizzie.stone@hotmail.com>
2020-04-02 15:36:55 +0100

    Add done printout

--- convert_dotmat_to_jason.m
+++ convert_dotmat_to_jason.m
@@ -1,5 +1,6 @@
 
-path = '/home/lizzie/OneDrive/data/collect_data_3d_varyAngle_FIXEDslice2019-10-01_1901/c45_01_20'; % a .mat file (but don't write .mat here)
+% path = '/home/lizzie/OneDrive/data/collect_data_3d_varyAngle_FIXEDslice2019-10-01_1901/c45_01_20'; % a .mat file (but don't write .mat here)
+path = 'C:\Users\ea-stone\Documents\data\collect_data_3d_varyAngle_FIXEDslice2019-10-01_1901\c30_01_15'; % a .mat file (but don't write .mat here)
 
 file_to_convert = strcat(path, '.mat');
 load(file_to_convert)
@@ -9,3 +10,4 @@ file_to_save_to = strcat(path, '.json');
 the_file = fopen(file_to_save_to ,'w');
 fprintf(the_file, jsonencode(data));
 fclose(the_file);
+disp("done")
