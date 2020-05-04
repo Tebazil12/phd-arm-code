6dd3e6b349c383a90e65f54cbf7f45875aefa6cd
Tebazil12 <lizzie.stone@hotmail.com>
2020-05-04 15:47:49 +0100

    Make function for finding max frame usable by ref and normal taps

--- offline_2D_transl_radii.py
+++ offline_2D_transl_radii.py
@@ -22,15 +22,17 @@ def load_data():
     return all_data
 
 
-def get_ref_tap(all_data):
-    ref_tap_all_frames = all_data[10 - 1][11 - 1] # -1 to translate properly from matlab
-
+def best_frame(all_frames):
+    """
+    For the given tap, select frame with the highest average pin displacement from the first frame.
+    Takes in one tap, in form (16ish x 126 ish x 2) np.array, returns (1 x 126ish x 2) np.array
+    """
     # make into pin displacement instead of absolute position
-    ref_tap_all_frames_disp = ref_tap_all_frames - ref_tap_all_frames[0]
+    all_frames_disp = all_frames - all_frames[0]
 
     # Find frame where largest pin displacement takes place (on average) #TODO this method is not the same as in MATLAB!
     # Average disps per frame (over all pins)
-    mean_disp_per_frame = np.mean(np.abs(ref_tap_all_frames_disp),axis=1)
+    mean_disp_per_frame = np.mean(np.abs(all_frames_disp),axis=1)
 
     # Find euclidean distance per frame
     distances_all_disps = np.linalg.norm(mean_disp_per_frame, axis=1)
@@ -39,8 +41,7 @@ def get_ref_tap(all_data):
     result = np.where(distances_all_disps == np.amax(distances_all_disps))
     max_frame_i = result[0][0]
 
-    ref_tap = ref_tap_all_frames_disp[max_frame_i]
-    return ref_tap
+    return all_frames_disp[max_frame_i]
 
 
 def get_training_data(all_data):
@@ -72,8 +73,8 @@ if __name__ == "__main__":
     all_data = load_data()
     # print(len(all_data[0]))
 
-    ref_tap = get_ref_tap(all_data)
+    ref_tap = best_frame(all_data[10 - 1][11 - 1]) # -1 to translate properly from matlab
     # print(ref_tap)
-    # print((ref_tap.shape))
+    print((ref_tap.shape))
 
     a = get_training_data(all_data)
